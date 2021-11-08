283c1735a39db653ab314affed9ca65987d7ab2b
Brandon Casamichana <brandoncasawork8@gmail.com>
2021-11-08 13:51:48 -0500

    made app a class

--- src/App.tsx
+++ src/App.tsx
@@ -11,30 +11,51 @@ import { CssBaseline, StyledEngineProvider } from "@mui/material";
 import { Provider, useSelector } from "react-redux";
 import themeDarkModeA from "./themes/DarkModeA";
 
-function App() {
-  return (
-    <ThemeProvider theme={themeDarkModeA}>
-      <CssBaseline />
-      <Router>
-        <div className="App">
-          <header>
-            <TopAppBar />
-          </header>
-          <main style={{ padding: 8, flexGrow: 1 }}>
-            <Route path="/" exact>
-              <HomePage />
-            </Route>
-            <Route path="/skillchat" exact>
-              Chatimus
-            </Route>
-          </main>
-          <footer>
-            <BottomNavBar />
-          </footer>
-        </div>
-      </Router>
-    </ThemeProvider>
-  );
+class App extends React.Component {
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      dbLoaded: false,
+    };
+  }
+
+  async dbLoad() {
+    this.setState({ dbLoaded: true });
+  }
+
+  componentDidMount() {
+    this.dbLoad();
+  }
+  render() {
+    return (
+      <ThemeProvider theme={themeDarkModeA}>
+        <CssBaseline />
+        <Router>
+          <div className="App">
+            <header>
+              <TopAppBar />
+            </header>
+            <main style={{ padding: 8, flexGrow: 1 }}>
+              <Route path="/" exact>
+                {
+                  // @ts-ignore
+                  this.state.dbLoaded.toString()
+                }
+                <HomePage />
+              </Route>
+              <Route path="/skillchat" exact>
+                Chatimus
+              </Route>
+            </main>
+            <footer>
+              <BottomNavBar />
+            </footer>
+          </div>
+        </Router>
+      </ThemeProvider>
+    );
+  }
 }
 
 export default App;
